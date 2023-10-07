3b3ed89861e2b22534c9fce213b6707987a107a7
ZauRoN <ustarkhanov.js@gmail.com>
2023-10-07 16:04:20 +0400

    2_6_Тесты_для_стороны_прямоугольника

---
+++ src/test/java/ru/condition/TrgAreaTest.java
@@ -0,0 +1,36 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        double expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+    @Test
+    void whenP8K5Square2Dot22() {
+        double expected = 2.22;
+        int p = 8;
+        double k = 5;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP10K3Square4Dot68() {
+        double expected = 4.68;
+        int p = 10;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+
+
+}
